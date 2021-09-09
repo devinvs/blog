bf3f358c669e2d09ffa88caa06b2220c971abcba
Devin Vander Stelt <devin@vstelt.com>
2021-09-09 12:06:52 -0400

    Here we go again...

--- src/main.rs
+++ src/main.rs
@@ -194,9 +194,8 @@ async fn main() -> std::io::Result<()> {
     } else {
         // Certificate crap
         let mut config = ServerConfig::new(NoClientAuth::new());
-        let cert_file = &mut BufReader::new(File::open("").unwrap());
-        let key_fiel = &mut BufReader::new(File::open("").unwrap());
-        let key_file = &mut BufReader::new(File::open("").unwrap());
+        let cert_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/void.vstelt.dev/cert.pem").unwrap());
+        let key_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/void.vstelt.dev/privkey.pem").unwrap());
         let cert_chain = certs(cert_file).unwrap();
         let mut keys = pkcs8_private_keys(key_file).unwrap();
         config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
